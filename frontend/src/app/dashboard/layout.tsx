a059b5d4f40f421c6ea2b2397feb2fc8f4da410a
jacksonhooi <113287892+jacksonhooi@users.noreply.github.com>
2025-10-10 17:44:55 +0800

    fix error

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import GlobalStyles from '@mui/material/GlobalStyles';
 
 import { AuthGuard } from '@/components/auth/auth-guard';
